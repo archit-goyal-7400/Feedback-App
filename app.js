0915496e8d5bf2e6ee4991d1f640a97f008ccb7c
archit828 <garchit2000@gmail.com>
2021-01-04 22:43:13 +0530

    google authentication added(step 1)

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const authRoutes = require("./routes/auth");
+const keys = require("./config/keys");
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientId,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken, refreshToken, profile);
+    }
+  )
+);
 
-app.get("/", (req, res) => {
-  res.send({
-    hello: "archit",
-  });
-});
+app.use(authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
