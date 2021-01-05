fad32b207744b6ede3067d14e2dc91f7108f152a
archit828 <garchit2000@gmail.com>
2021-01-05 12:21:11 +0530

    google authentication added

--- app.js
+++ app.js
@@ -4,6 +4,32 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const authRoutes = require("./routes/auth");
 const keys = require("./config/keys");
+const mongoose = require("mongoose");
+const User = require("./models/user");
+const cookieSession = require("cookie-session");
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieSecret],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+  console.log(user.id, "serialize");
+});
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then((user) => {
+      done(null, user);
+      console.log(user, "deserialize");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 passport.use(
   new GoogleStrategy(
     {
@@ -13,6 +39,15 @@ passport.use(
     },
     (accessToken, refreshToken, profile, done) => {
       console.log(accessToken, refreshToken, profile);
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (!existingUser) {
+          new User({ googleId: profile.id }).save().then((user) => {
+            done(null, user);
+          });
+        } else {
+          done(null, existingUser);
+        }
+      });
     }
   )
 );
@@ -20,4 +55,7 @@ passport.use(
 app.use(authRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+mongoose.connect(keys.mongoURI).then(() => {
+  app.listen(PORT);
+  console.log("server is up at ", PORT);
+});
--- routes/auth.js
+++ routes/auth.js
@@ -9,5 +9,15 @@ router.get(
 );
 
 router.get("/auth/google/callback", passport.authenticate("google"));
+router.get("/api/logout", (req, res) => {
+  req.logout();
+  res.send(req.user);
+});
+
+//For testing purpose
+
+router.get("/api/current_user", (req, res) => {
+  res.send(req.user);
+});
 
 module.exports = router;
