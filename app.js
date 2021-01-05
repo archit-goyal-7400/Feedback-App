0462cbaec3f78e7e13ef81589e8fd24979acb3ed
archit828 <garchit2000@gmail.com>
2021-01-05 12:27:40 +0530

    Code refactoring

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const authRoutes = require("./routes/auth");
 const keys = require("./config/keys");
 const mongoose = require("mongoose");
 const User = require("./models/user");
 const cookieSession = require("cookie-session");
+
+require("./services/passport");
 
 app.use(
   cookieSession({
@@ -16,41 +17,6 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-  console.log(user.id, "serialize");
-});
-passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then((user) => {
-      done(null, user);
-      console.log(user, "deserialize");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientId,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken, refreshToken, profile);
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if (!existingUser) {
-          new User({ googleId: profile.id }).save().then((user) => {
-            done(null, user);
-          });
-        } else {
-          done(null, existingUser);
-        }
-      });
-    }
-  )
-);
 
 app.use(authRoutes);
 
---
+++ services/passport.js
@@ -0,0 +1,40 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/user");
+const keys = require("../config/keys");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientId,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // console.log(accessToken, refreshToken, profile);
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (!existingUser) {
+          new User({ googleId: profile.id }).save().then((user) => {
+            done(null, user);
+          });
+        } else {
+          done(null, existingUser);
+        }
+      });
+    }
+  )
+);
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+  // console.log(user.id, "serialize");
+});
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then((user) => {
+      done(null, user);
+      // console.log(user, "deserialize");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
