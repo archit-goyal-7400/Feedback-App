01af299228d932d04a95c5ac6bd6ef6104bf06a0
archit828 <garchit2000@gmail.com>
2021-01-07 12:13:02 +0530

    Initial client side added

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+const App = (props) => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Route></Route>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
