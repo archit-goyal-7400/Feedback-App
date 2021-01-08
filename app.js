bb080de4b2f58c4880c9ca8c122b27c62676f784
archit828 <garchit2000@gmail.com>
2021-01-08 12:56:36 +0530

    Setting config for deployment

--- app.js
+++ app.js
@@ -23,6 +23,15 @@ app.use(passport.session());
 app.use(authRoutes);
 app.use(billingRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 mongoose.connect(keys.mongoURI).then(() => {
   app.listen(PORT);
